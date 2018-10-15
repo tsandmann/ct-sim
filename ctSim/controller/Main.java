0b565b005d8b9a3febe60aaba01a822a62f6bf8f
Timo Sandmann <mail@timosandmann.de>
2018-10-15 21:17:17 +0200

    version bump to 2.27

--- ctSim/controller/Main.java
+++ ctSim/controller/Main.java
@@ -52,7 +52,7 @@ import ctSim.view.gui.SplashWindow;
  */
 public class Main {
 	/** Versionsnummer */
-	public static final String VERSION = "2.26";
+	public static final String VERSION = "2.27";
 	/** Konfigurationsdatei */
 	private static final String DEFAULT_CONFIGFILE = "config/ct-sim.xml";
     /** Flag, welches die Anzeige des Splashscreens bewirkt (DEFAULT: TRUE) */
